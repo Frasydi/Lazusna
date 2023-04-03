0f6f04b75df3f634b600ed7b42023a5e7fe2015f
Frasydi <Frasydi137@Gmail.com>
2023-04-03 18:15:34 +0000

    fi

--- fullstack/server/server.js
+++ fullstack/server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8001;
 const cors = require("cors")
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
